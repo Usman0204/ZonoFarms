5da06c767273733cc5099134c95df478d5c0e2c7
Usman Aslam Malik <usman.maliknu13@gmail.com>
2021-09-22 12:58:29 -0400

    first commit

---
+++ src/hooks/useGetDocumentTitlePrice.ts
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+import { usePriceCakeBusd } from 'state/hooks'
+
+const useGetDocumentTitlePrice = () => {
+  const cakePriceUsd = usePriceCakeBusd()
+  const currentPrice = 0.0
+  useEffect(() => {
+    document.title = `ZonoSwap - $${Number(currentPrice).toLocaleString(undefined, {
+      minimumFractionDigits: 3,
+      maximumFractionDigits: 3,
+    })}`
+  })
+}
+export default useGetDocumentTitlePrice
